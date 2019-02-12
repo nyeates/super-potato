17da72459b529cb5f866c495a47c8763a55d078f
root <root@ma241.mars.fastwebserver.de>
2019-02-12 01:27:11 +0100

    remove old files

--- motdOff.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo -e "*****\nMOTD TURN OFF (message upon SSH login)\n*****\n\n"
-if ((${EUID:-0} || "$(id -u)")); then echo "ERROR: Needs to be run as root."; exit 100; fi
-
-chmod -x /etc/update-motd.d/*
-
-echo -e "\n*****\nMOTD turned off\nTo turn MOTD back on: sudo chmod +x /etc/update-motd.d/*\n*****"
