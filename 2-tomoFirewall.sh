c2e88ff79a5488ed4583d2e5c40e7f267adc31f5
Nick Yeates <213291+nyeates@users.noreply.github.com>
2019-02-18 10:26:36 -0500

    Added status messages when operating

--- 2-tomoFirewall.sh
+++ 2-tomoFirewall.sh
@@ -5,9 +5,11 @@ if ((${EUID:-0} || "$(id -u)")); then echo "ERROR: Needs to be run as root."; ex
 # Tomo port - used for talking to and from other node clients
 port="30303"
 
+echo -e "\n***** Install Firewall (ufw)"
 # Install needed packages
 apt install ufw
 
+echo -e "\n***** Configuring firewall ports specific to Tomo"
 # Configure firewall
 ufw allow $port/tcp # add tomo port
 ufw allow $port/udp # add tomo port
