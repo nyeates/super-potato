c44cb05b06f774674a6894bd5ddb4f7359c5d4d7
Nick Yeates <213291+nyeates@users.noreply.github.com>
2019-02-26 04:37:50 -0500

    first version

---
+++ 6-ctmInstall.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo -e "\n*****\nDOCKER COMPOSE\n*****\n"
+if ((${EUID:-0} || "$(id -u)")); then echo "Running as user: $EUID $id"; else echo "ERROR: Needs to be run as NON ROOT."; exit 100; fi
+
+echo -e "\n***** download docker-compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+echo -e "Press Enter... "; read shit
+
+echo -e "\n***** Installing Docker-Compose"
+sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/1.23.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+docker-compose version
+echo -e "Press Enter... "; read shit
+
+echo -e "\n***** Installing and updating create-tomochain-masternode pip3 package"
+pip3 install --user create-tomochain-masternode
+pip3 install -U create-tomochain-masternode
+echo -e "Press Enter... "; read shit
+
+#echo -e "\n***** Tmn Details:"
+#pip3 show tmn
+
+#echo -e "\n***** Assure \$PATH env-variable contains /home/[user]/.local/bin :"
+#echo "export PATH=$PATH:$HOME/.local/bin" >> $HOME/.bashrc
+#source $HOME/.bashrc
+#echo $PATH
+#echo -e "Press Enter... "; read shit
+
+echo -e "\n*****\ncreate-tomochain-masternode has been installed\nNext steps:\n"
+echo -e "Run create-tomochain-masternode with the name of your masternode as argument:"
+echo -e "create-tomochain-masternode mymasternodetmn\n"
+echo -e "docker exec \$(docker ps -q -f \"name=tomochain\") tomo attach /tomochain/data/tomo.ipc --exec \"eth.blockNumber\""
+echo -e "docker exec \$(docker ps -q -f \"name=tomochain\") tomo attach /tomochain/data/tomo.ipc --exec \"net.peerCount\"\n*****"
