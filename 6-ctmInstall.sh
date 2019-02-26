5cfbd64c5c27eb52ebe50991e20251128bf2f90c
Nick Yeates <213291+nyeates@users.noreply.github.com>
2019-02-26 04:59:11 -0500

    second version

--- 6-ctmInstall.sh
+++ 6-ctmInstall.sh
@@ -6,6 +6,13 @@ echo -e "\n***** download docker-compose"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 echo -e "Press Enter... "; read shit
 
+echo -e "\n***** Installing Pre-req Packages"
+sudo apt install python3-setuptools
+pip3 install wheel
+# sudo apt install build-essential
+# sudo apt install python3-dev
+echo -e "Press Enter... "; read shit
+
 echo -e "\n***** Installing Docker-Compose"
 sudo chmod +x /usr/local/bin/docker-compose
 sudo curl -L https://raw.githubusercontent.com/docker/compose/1.23.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
@@ -17,17 +24,18 @@ pip3 install --user create-tomochain-masternode
 pip3 install -U create-tomochain-masternode
 echo -e "Press Enter... "; read shit
 
-#echo -e "\n***** Tmn Details:"
-#pip3 show tmn
+echo -e "\n***** create-tomochain-masternode Details:"
+pip3 show create-tomochain-masternode
+echo -e "Press Enter... "; read shit
 
-#echo -e "\n***** Assure \$PATH env-variable contains /home/[user]/.local/bin :"
-#echo "export PATH=$PATH:$HOME/.local/bin" >> $HOME/.bashrc
-#source $HOME/.bashrc
-#echo $PATH
-#echo -e "Press Enter... "; read shit
+echo -e "\n***** Assure \$PATH env-variable contains /home/[user]/.local/bin :"
+echo "export PATH=$PATH:$HOME/.local/bin" >> $HOME/.bashrc
+source $HOME/.bashrc
+echo $PATH
+echo -e "Press Enter... "; read shit
 
 echo -e "\n*****\ncreate-tomochain-masternode has been installed\nNext steps:\n"
 echo -e "Run create-tomochain-masternode with the name of your masternode as argument:"
-echo -e "create-tomochain-masternode mymasternodetmn\n"
+echo -e "\033[1mcreate-tomochain-masternode Atlantis\033[0m\n"
 echo -e "docker exec \$(docker ps -q -f \"name=tomochain\") tomo attach /tomochain/data/tomo.ipc --exec \"eth.blockNumber\""
 echo -e "docker exec \$(docker ps -q -f \"name=tomochain\") tomo attach /tomochain/data/tomo.ipc --exec \"net.peerCount\"\n*****"
