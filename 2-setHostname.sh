3fc5c9b61119b813557267941bc19c19b82d9b65
N <213291+nyeates@users.noreply.github.com>
2019-03-12 04:38:56 +0100

    created Hostname file so that we can rename servers

---
+++ 2-setHostname.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo -e "\n*****\nSET HOSTNAME\n*****\n"
+if ((${EUID:-0} || "$(id -u)")); then echo "ERROR: Needs to be run as root."; exit 100; fi
+
+echo -e "***** Set Hostname"
+echo -e "Enter the hostname you would like to use: "
+read newHostname
+hostnamectl set-hostname $newHostname
+
+# Add new hostname to /etc/hosts
+echo -e "$ vi /etc/hosts\n"
+$oldHostname = $(hostname -s)
+vim -c ':2s/\(.\{-}\zs'"$oldHostname"'\)\{2}/'"$newHostname"'/' -c 'wq' /etc/hosts
+echo -e "Press Enter... "; read shit
+
+# Restart SSHD service
+echo -e "\n*****\nHostname Changed to $newHostname"
+echo -e "Press ENTER to RESTART... "
+read restart
+reboot
