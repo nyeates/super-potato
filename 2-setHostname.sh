254e231460801c44cb8bccf8359e63665786f0d8
N <213291+nyeates@users.noreply.github.com>
2019-03-12 10:43:17 +0100

    dunno

--- 2-setHostname.sh
+++ 2-setHostname.sh
@@ -2,6 +2,8 @@
 echo -e "\n*****\nSET HOSTNAME\n*****\n"
 if ((${EUID:-0} || "$(id -u)")); then echo "ERROR: Needs to be run as root."; exit 100; fi
 
+$oldHostname = $(hostname -s)
+
 echo -e "***** Set Hostname"
 echo -e "Enter the hostname you would like to use: "
 read newHostname
@@ -9,7 +11,6 @@ hostnamectl set-hostname $newHostname
 
 # Add new hostname to /etc/hosts
 echo -e "$ vi /etc/hosts\n"
-$oldHostname = $(hostname -s)
 # https://askubuntu.com/questions/950017/how-to-substitute-the-nth-occurrence-pattern-in-vi-editor
 vim -c ':2s/\(.\{-}\zs'"$oldHostname"'\)\{2}/'"$newHostname"'/' -c 'wq' /etc/hosts
 echo -e "Press Enter... "; read shit
