08371a565111547206d9b0e4d382033dea43ec66
Nick Yeates <213291+nyeates@users.noreply.github.com>
2019-07-15 16:20:13 -0400

    Changed to let user edit /etc/hosts manually
    Some hosts files had a second line, others didnt; depended on image or linux flavor used; so instead of automatically changing the file, allow user to do the editing

--- 2-setHostname.sh
+++ 2-setHostname.sh
@@ -13,12 +13,20 @@ hostnamectl set-hostname $newHostname
 echo -e "\n$ hostnamectl set-hostname $newHostname\nPress Enter... "; read shit
 
 # Add new hostname to /etc/hosts
-echo -e "$ vi /etc/hosts"
+echo -e "YOU WILL NEXT ENTER VI TEXT EDITOR AND ADD/EDIT A LINE FOR THE NEW HOSTNAME\n"
+echo -e "THE 2nd LINE NEEDS TO LOOK SIMILAR TO BELOW. YOU MAY WANT TO COPY THE 2nd LINE, SO THAT YOU CAN REPLICATE IT."
+echo -e "To edit, write, and close vi, you will need to know its commands..."
+echo -e "================="
+echo -e "127.0.0.1       localhost"
+echo -e "78.31.64.253    vps1836265.fastwebserver.de bb-8"
+echo -e "================="
+echo -e "$ vim /etc/hosts"
 echo -e "Press Enter... "; read shit
 
 # https://askubuntu.com/questions/950017/how-to-substitute-the-nth-occurrence-pattern-in-vi-editor
 #vim -c ':2s/\(.\{-}\zs'"$oldHostname"'\)\{2}/'"$newHostname"'/' -c 'wq' /etc/hosts
-sudo vim -c ':2s/\(.\{-}\zs'$oldHostname'\)\{2}/'$newHostname'/' -c 'wq' /etc/hosts
+#sudo vim -c ':2s/\(.\{-}\zs'$oldHostname'\)\{2}/'$newHostname'/' -c 'wq' /etc/hosts
+sudo vim /etc/hosts
 
 # Show /etc/hosts file
 echo -e "\n***** /etc/hosts file:"
