746ee96184ff75d87d67e75fcfdb8c98be4a54c4
Nick Yeates <213291+nyeates@users.noreply.github.com>
2019-07-16 13:26:18 -0400

    Add prompts
    In case an error occurs

--- 7-pythonAndDocker.sh
+++ 7-pythonAndDocker.sh
@@ -4,25 +4,36 @@ if ((${EUID:-0} || "$(id -u)")); then echo "Running as user: $EUID $id"; else ec
 
 echo -e "\n***** Intall required packages"
 # Might need these
-sudo apt install dirmngr gpg-agent gnupg-agent
+sudo apt install dirmngr
+sudo apt install gnupg-agent
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 7EA0A9C3F273FCD8
+echo -e "Press Enter..."
+read shit
 
 echo -e "\n***** Install Python3"
 sudo apt install python3
 sudo apt install python3-pip
+echo -e "Press Enter..."
+read shit
 
 echo -e "\n***** Install pre-req packages"
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo -e "Press Enter..."
+read shit
 
 echo -e "\n***** Check fingerprint and setup 'stable' repo"
 apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+echo -e "Press Enter..."
+read shit
 
 echo -e "\n***** Install Docker-ce"
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io
+echo -e "Press Enter..."
+read shit
 
 echo -e "\n***** Add user to docker group"
 echo -e "Enter user name to add to docker: "
@@ -33,6 +44,8 @@ sudo usermod -aG docker $user
 cat /etc/group | grep docker
 groups $user
 echo -e "*****\n"
+echo -e "Press Enter..."
+read shit
 
 echo -e "\n***** Run Hello World Test"
 docker run hello-world
