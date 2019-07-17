fa92f8a217693a052e663dcce7acf5f4443ad118
Nick Yeates <213291+nyeates@users.noreply.github.com>
2019-07-17 16:48:33 -0400

    Help structure and VM mods
    Modded commands to work on new VMs; added structure from github file that allows passing of  "?" when running the script, so that you can see help commands

--- 8-ctmInstall.sh
+++ 8-ctmInstall.sh
@@ -1,42 +1,80 @@
 #!/bin/bash
-echo -e "\n*****\nDOCKER COMPOSE\n*****\n"
-if ((${EUID:-0} || "$(id -u)")); then echo "Running as user: $EUID $id"; else echo "ERROR: Needs to be run as NON ROOT."; exit 100; fi
 
-echo -e "\n***** download docker-compose"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-echo -e "Press Enter... "; read shit
+# See Main Execution below the following functions...
 
-echo -e "\n***** Installing Pre-req Packages"
-sudo apt install python3-setuptools
-sudo apt install python-pip
-pip3 install wheel
-# sudo apt install build-essential
-# sudo apt install python3-dev
-echo -e "Press Enter... "; read shit
+# Just display helpful commands
+display_help ()
+{
+	echo -e "*****"
+	echo -e "Important docker-compose commands:"
+	echo -e ""
+	echo -e ""
+	echo -e ""
+	echo -e ""
+	echo -e ""
+	echo -e ""
+	echo -e ""
+	echo -e "*****"
+}
 
-echo -e "\n***** Installing Docker-Compose"
-sudo chmod +x /usr/local/bin/docker-compose
-sudo curl -L https://raw.githubusercontent.com/docker/compose/1.23.2/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
-docker-compose version
-echo -e "Press Enter... "; read shit
+# Install and prepare docker-compose tool
+run_script ()
+{
+  # from https://docs.docker.com/compose/install/
+  echo -e "\n*****\nDOCKER COMPOSE\n*****\n"
+  if ((${EUID:-0} || "$(id -u)")); then echo "Running as user: $EUID $id"; else echo "ERROR: Needs to be run as NON ROOT."; exit 100; fi
 
-echo -e "\n***** Installing and updating create-tomochain-masternode pip3 package"
-pip3 install --user create-tomochain-masternode
-pip3 install -U create-tomochain-masternode
-echo -e "Press Enter... "; read shit
+  echo -e "\n***** download docker-compose"
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  echo -e "Press Enter... "; read shit
 
-echo -e "\n***** create-tomochain-masternode Details:"
-pip3 show create-tomochain-masternode
-echo -e "Press Enter... "; read shit
+  echo -e "\n***** Installing Pre-req Packages"
+  sudo apt install python3-setuptools
+  sudo apt install python-pip
+  pip3 install wheel
+  # sudo apt install build-essential
+  # sudo apt install python3-dev
+  echo -e "Press Enter... "; read shit
 
-echo -e "\n***** Assure \$PATH env-variable contains /home/[user]/.local/bin :"
-echo "export PATH=$PATH:$HOME/.local/bin" >> $HOME/.bashrc
-source $HOME/.bashrc
-echo $PATH
-echo -e "Press Enter... "; read shit
+  echo -e "\n***** Installing Docker-Compose"
+  sudo chmod +x /usr/local/bin/docker-compose
+  # Allow tab-completion of docker-compose commands
+  sudo curl -L https://raw.githubusercontent.com/docker/compose/1.24.1/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+  docker-compose --version
+  echo -e "Press Enter... "; read shit
 
-echo -e "\n*****\ncreate-tomochain-masternode has been installed\nNext steps:\n"
-echo -e "Run create-tomochain-masternode with the name of your masternode as argument:"
-echo -e "\033[1mcreate-tomochain-masternode Atlantis\033[0m\n"
-echo -e "docker exec \$(docker ps -q -f \"name=tomochain\") tomo attach /tomochain/data/tomo.ipc --exec \"eth.blockNumber\""
-echo -e "docker exec \$(docker ps -q -f \"name=tomochain\") tomo attach /tomochain/data/tomo.ipc --exec \"net.peerCount\"\n*****"
+  echo -e "\n***** Installing and updating create-tomochain-masternode pip3 package"
+  pip3 install --user create-tomochain-masternode
+  # Upgrade CTM pip install (in case new version has come out)
+  pip3 install -U create-tomochain-masternode
+  echo -e "Press Enter... "; read shit
+
+  echo -e "\n***** create-tomochain-masternode Details:"
+  pip3 show create-tomochain-masternode
+  echo -e "Press Enter... "; read shit
+
+  echo -e "\n***** Assure \$PATH env-variable contains /home/[user]/.local/bin :"
+  echo "export PATH=$PATH:$HOME/.local/bin" >> $HOME/.bashrc
+  source $HOME/.bashrc
+  echo $PATH
+  echo -e "Press Enter... "; read shit
+
+  echo -e "\n*****\ncreate-tomochain-masternode has been installed\nNext steps:\n"
+  echo -e "Run create-tomochain-masternode with the name of your masternode as argument:"
+  echo -e "\033[1mcreate-tomochain-masternode Atlantis\033[0m\n"
+  echo -e "docker exec \$(docker ps -q -f \"name=tomochain\") tomo attach /tomochain/data/tomo.ipc --exec \"eth.blockNumber\""
+  echo -e "docker exec \$(docker ps -q -f \"name=tomochain\") tomo attach /tomochain/data/tomo.ipc --exec \"net.peerCount\"\n*****"
+}
+
+# Main Execution
+# If an argument containing "?" is passed when this shell script is run
+# This allows us to run as "0-githubLink.sh ?" and get only the command info back
+# https://superuser.com/questions/186272/check-if-any-of-the-parameters-to-a-bash-script-match-a-string
+
+if [[ $@ =~ "?" ]]; then
+        display_help
+        exit
+else
+        run_script
+        exit
+fi
