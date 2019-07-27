8aa677067a5741712f02bfb54709855ed73ac7db
N <213291+nyeates@users.noreply.github.com>
2019-07-27 09:23:43 +0200

    dunno

--- 1-sshdPortChange.sh
+++ 1-sshdPortChange.sh
@@ -11,8 +11,12 @@ apt install ufw
 
 echo -e "\n***** Changing SSHD port in config file"
 echo -e "$ vim /etc/ssh/sshd_config"
+echo -e "YOU WILL WANT TO ASSURE THAT THE PORT IN THE FILE ACTUALLY GETS AUTO-CHANED TO $port."
+echo -e "Sometimes it doesnt because formatting is not as expected."
+echo -e "Press Enter... "; read shit
 # Change SSHD port in config file
 vim -c '%s/Port 22/Port '"$port"'/c' -c 'wq' /etc/ssh/sshd_config
+vi /etc/ssh/sshd_config
 
 echo -e "\n***** Setting up firewall"
 # Setup firewall
@@ -20,6 +24,7 @@ ufw allow $port/tcp
 ufw limit $port/tcp
 ufw delete limit ssh # remove old ssh port if it was there
 ufw logging on
+echo -e "Press Enter... "; read shit
 
 echo -e "\n***** Restarting Firewall (ufw) and SSH service"
 ufw enable
